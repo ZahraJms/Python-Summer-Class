8acefcf7411b2da23385f6d77e5b7ab38391ad84
ZahraJms <68468295+ZahraJms@users.noreply.github.com>
2020-07-22 18:18:05 +0430

    Update Ex3 _ Zahra Jamshidi.py

--- Ex3 _ Zahra Jamshidi.py
+++ Ex3 _ Zahra Jamshidi.py
@@ -2,8 +2,6 @@ year=int(input("Enter The Year"))
 if year%4==0:
     if year%400==0 or year%100!=0:
         print("Leap Year")
-    elif year%100==0:
-        print("Not Leap Year")
     else :
         print("Not Leap Year")
 else :
