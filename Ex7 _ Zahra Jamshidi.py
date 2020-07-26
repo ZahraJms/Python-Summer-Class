2d07263bd36b8dd4684b9b50130499156352a3fc
ZahraJms <68468295+ZahraJms@users.noreply.github.com>
2020-07-27 00:40:41 +0430

    Update Ex7 _ Zahra Jamshidi.py

--- Ex7 _ Zahra Jamshidi.py
+++ Ex7 _ Zahra Jamshidi.py
@@ -1,11 +1,11 @@
-def fib(n):
-    if n==1:
-        return 0
-    elif n==2:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
 def fiblist(n):
+    def fib(n):
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
     fiblist=[]
     for i in range(1,n+1):
         fiblist.append(fib(i))
